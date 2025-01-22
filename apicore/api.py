1c221fea9ac8c88def113b59022788295a7fae6f
Franco BC <francobc10@gmail.com>
2025-01-22 00:15:32 -0300

    Delete comment

--- apicore/api.py
+++ apicore/api.py
@@ -16,7 +16,7 @@ db = SQLAlchemy(app)
 # Define a User model
 class User(db.Model):
    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-   api_key = db.Column(db.String(36), default=lambda: str(uuid.uuid4()))  # TODO sumarlo como query param
+   api_key = db.Column(db.String(36), default=lambda: str(uuid.uuid4()))
    type = db.Column(db.String(80), nullable=False)
    username = db.Column(db.String(80), nullable=False, unique=True)
 
