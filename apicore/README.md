1248c8086396d5dc3d8b9a94fb67f3a54cb6324d
Franco BC <francobc10@gmail.com>
2025-01-22 00:20:55 -0300

    Add rest client integration and cache service

--- apicore/README.md
+++ apicore/README.md
@@ -7,4 +7,11 @@ python3 -m venv .venv
 . .venv/bin/activate
 pip install Flask
 pip install Flask-Limiter
+pip install simple-rest-client
+pip install pymemcache
+
+arch -arm64 brew install memcached
+brew install memcached
+brew services start memcached
+
 flask --app hello run
--- apicore/api.py
+++ apicore/api.py
@@ -2,93 +2,93 @@ from flask import Flask, request, jsonify
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_limiter.errors import RateLimitExceeded
+from pymemcache.client import base
+from simple_rest_client.api import API
 
 
 app = Flask(__name__)
 
+# Create api instance
+api = API(
+    api_root_url='http://localhost:5001/',  # base api url
+    params={},  # default params
+    headers={},  # default headers
+    timeout=2,  # default timeout in seconds
+    append_slash=False,  # append slash to final url
+    json_encode_body=True,  # encode body as json
+)
+api.add_resource(resource_name='users')
 
 # Initialize the Limiter
 limiter = Limiter(
-   key_func=lambda: (request.headers.get('Authorization') or get_remote_address()),  # Falling back to IP if header is missing
+   key_func=lambda: (request.headers.get('Authorization') or get_remote_address()),
+   # Falling back to IP if header is missing
    app=app
 )
 
+# Initialize the Cache
+client = base.Client(('localhost', 11211))
 
 
-
+# START Rate Limit Section
 def get_user_type_rate_limit():
-   user_type = request.headers.get('X-User-Type', 'guest')  # Default to 'guest' if not provided
+   # Access headers
+   authorization = request.headers.get('Authorization')
+   # Try search in cache
+   user_type = client.get(authorization)
 
+   if user_type is None:  # Cache failed and set value
+       request_params = {'api_key': authorization,
+                         'access_token': 'valid-token'}  # TODO podríamos usar key para sumar seguridad entre servicios
+       response = api.users.list(body=None, params=request_params, headers={})
+       if not response.body:
+           user_type = "NOT_REGISTERED"
+       else:
+           user_type = response.body[0]['type']
+       client.set(authorization, user_type, expire=60)  # 60 seconds
+   else:
+       user_type = user_type.decode('ASCII')
 
    if user_type == 'PREMIUM':
        return "5 per minute"
    elif user_type == 'FREEMIUM':
        return "3 per minute"
+   elif user_type == 'NOT_REGISTERED':
+       return "0 per minute"
    else:
        return "1 per minute"
 
 
-
-
-@app.errorhandler(RateLimitExceeded)
-def handle_ratelimit_error(e):
-   return jsonify(error="Usted posee el plan FREEMIUM, puede realizar como máximo 5 solicitudes por minuto (RPM). "
-                        "Actualice el plan a PREEMIUM o espere unos momentos, gracias!"), 429
-
-
-
-
 @app.errorhandler(RateLimitExceeded)
 def handle_ratelimit_exceeded(e):
-   user_type = request.headers.get('X-User-Type', 'guest')
-
+   # Access headers
+   authorization = request.headers.get('Authorization')
+   user_type = client.get(authorization).decode('ASCII')
 
    if user_type == 'PREMIUM':
-       message = "Has superado tu límite de solicitudes por minuto. Como usuario PREMIUM, puedes esperar un momento " \
-                 "y volver a intentarlo, gracias."
+       message = "Has superado tu límite de solicitudes por minuto como usuario PREMIUM, debes esperar un momento."
    elif user_type == 'FREEMIUM':
-       message = "Has superado tu límite de solicitudes por minuto como usuario FREEMIUM. Actualiza a PREMIUM para " \
-                 "realizar más solicitudes o vuelve a intentarlo más tarde, gracias."
+       message = "Has superado tu límite de solicitudes por minuto como usuario FREEMIUM, debes esperar un momento."
+   elif user_type == 'NOT_REGISTERED':
+       return jsonify({'error': "El usuario no se encuentra registrado."}), 400
    else:
-       message = "Has superado tu límite de solicitudes por minuto. Considera contratar un plan para mas accesos o " \
-                 "espera un momento y vuelve a intetar mas tarde, gracias."
-
+       message = "Has superado tu límite de solicitudes por minuto. Considera contratar un plan para mas accesos."
 
    return jsonify({'error': message}), 429
-
-
-
-
-@app.route("/")
-@limiter.limit("3 per minute")  # Specific limit for this endpoint
-def hello_world():
-   return "<p>Hello, World!</p>"
-
-
+# END Rate Limit Section
 
 
 @app.route('/service', methods=['POST'])
 @limiter.limit(lambda: get_user_type_rate_limit())
 def service():
-   # Access headers
    authorization = request.headers.get('Authorization')
-   # Retrieve JSON data from the request
    data = request.get_json()
-
 
    if not data:
        return jsonify({'error': 'Invalid or missing JSON data'}), 400
-
-
-   # Access properties within the JSON data
-   apartments_list = data.get('inputs')
-   if not apartments_list:
-       return jsonify({'error': 'Missing required fields'}), 400
-
-
-   # Example operations
-   print(f"Apartments: {apartments_list}")
-   print(f"Authorization: {authorization}")
-
+   else:
+       apartments_list = data.get('inputs')
+       if not apartments_list:
+           return jsonify({'error': 'Missing required fields'}), 400
 
    return jsonify({'apartments_list': apartments_list, 'Authorization': authorization}), 200
