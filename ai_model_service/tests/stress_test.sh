2c899a0319cb2bf00cd4494a98a601dc43d39e7d
Nicolas Madeo <nicomad.17@gmail.com>
2025-02-03 23:09:59 -0300

    ai model stress tests fix

--- ai_model_service/tests/stress_test.sh
+++ ai_model_service/tests/stress_test.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+# List of real_state_index values
+INDEXES=(
+  393231
+  360467
+  393246
+  294943
+  294945
+  294947
+  393251
+  294951
+  393255
+)
 
-make_request(){
-    curl http://localhost:5000/predict -H 'Content-Type: application/json' -s -d '{"real_state_index": 412734 }'
+# Function to make a request with a random index from the list
+make_request() {
+    local random_index=${INDEXES[$RANDOM % ${#INDEXES[@]}]}
+    curl http://localhost:5000/predict -H 'Content-Type: application/json' -s -d "{\"real_state_index\": $random_index }"
 }
 
-if [[ -n $1 ]];
-then
+# Check if number of requests is provided
+if [[ -n $1 ]]; then
     echo "Stress test started with $1 requests"
 else
     echo "Usage: $0 <number_of_requests>"
     exit 1
 fi
 
-
-for i in $(seq 1 $1);
-do
-    make_request $2 &
+# Loop for making requests in parallel
+for i in $(seq 1 $1); do
+    make_request &
 done
 
 echo "Stress test ended"
