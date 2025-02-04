1ba166e4f5bde8e2271741c24ac65e7fa41c650c
Nicolas Madeo <nicomad.17@gmail.com>
2025-02-04 01:30:46 -0300

    ai model service log util update

--- ai_model_service/app/utils.py
+++ ai_model_service/app/utils.py
@@ -3,13 +3,17 @@ import requests
 from os import environ
 
 BASE_LOG_URL = environ.get('LOGGER_SERVICE_BASE_URL', 'http://localhost:5000')
+SERVICE_NAME = environ.get('SERVICE_NAME')
+
+if not SERVICE_NAME:
+    raise ValueError("SERVICE_NAME environment variable must be set")
 
 print(f"Using LOGGER_SERVICE_BASE_URL: {BASE_LOG_URL}")
 
 
 def log(message: str, tags: list[tuple[str,str]] = None) -> None:
     body = {
-        'service': 'prediction_service',
+        'service': SERVICE_NAME,
         'message': message,
         'tag_timestamp': datetime.utcnow().isoformat(),
         'tag_type': 'prediction'
